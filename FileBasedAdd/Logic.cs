d0b860692b1e3aff07eb1efbd0966f85a0880af4
GET Academy <terje@kolderup.net>
2026-05-28 09:40:18 +0200

    x

---
+++ FileBasedAdd/Logic.cs
@@ -0,0 +1,140 @@
+﻿namespace FileBasedAdd
+{
+    record FileNames(string InputFile1, string InputFile2, string OutputFile);
+
+    internal class Logic
+    {
+        public static Result<FileNames> GetArgs(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                return Result<FileNames>.Failure(
+                    "Feil: Du må angi nøyaktig tre filnavn.\n" +
+                    "Bruk: dotnet run <tallfil1> <tallfil2> <resultatfil>");
+            }
+
+            return Result<FileNames>.Success(
+                new FileNames(
+                    args[0], args[1], args[2]));
+        }
+
+        public static Func<int,Result<int>> ReadFileAndParseIntAndAdd(string fileName)
+        {
+            return (int total) =>
+            {
+                try
+                {
+                    string text1 = File.ReadAllText(fileName).Trim();
+
+                    if (!int.TryParse(text1, out int number))
+                    {
+                        return Result<int>.Failure(
+                            $"Feil: Innholdet i '{fileName}' er ikke et gyldig heltall.\n" +
+                            $"Innhold: '{text1}'");
+                    }
+
+                    return Result<int>.Success(number + total);
+                }
+                catch (FileNotFoundException)
+                {
+                    return Result<int>.Failure($"Feil: Fant ikke filen '{fileName}'.");
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    return Result<int>.Failure($"Feil: Har ikke tilgang til å lese filen '{fileName}'.");
+                }
+                catch (IOException ex)
+                {
+                    return Result<int>.Failure($"Feil: Klarte ikke å lese filen '{fileName}'.\n" + ex.Message);
+                }
+            };
+        }
+
+        /*
+            public static Result<int> ReadFileAndParseIntAndAdd(string fileName)
+           {
+               try
+               {
+                   string text1 = File.ReadAllText(fileName).Trim();
+
+                   if (!int.TryParse(text1, out int number))
+                   {
+                       return Result<int>.Failure(
+                           $"Feil: Innholdet i '{fileName}' er ikke et gyldig heltall.\n" +
+                           $"Innhold: '{text1}'");
+                   }
+
+                   return Result<int>.Success(number + total);
+               }
+               catch (FileNotFoundException)
+               {
+                   return Result<int>.Failure($"Feil: Fant ikke filen '{fileName}'.");
+               }
+               catch (UnauthorizedAccessException)
+               {
+                   return Result<int>.Failure($"Feil: Har ikke tilgang til å lese filen '{fileName}'.");
+               }
+               catch (IOException ex)
+               {
+                   return Result<int>.Failure($"Feil: Klarte ikke å lese filen '{fileName}'.\n" + ex.Message);
+               }
+           }         
+         */
+
+        public static Func<int,Result<int>> SaveFile(string fileName)
+        {
+            return (int number) =>
+            {
+                try
+                {
+                    if (File.Exists(fileName))
+                    {
+                        return Result<int>.Failure($"Feil: Utfilen '{fileName}' finnes allerede.");
+                    }
+
+                    File.WriteAllText(fileName, number.ToString());
+
+                    return Result<int>.Success(number);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    return Result<int>.Failure($"Feil: Har ikke tilgang til å skrive til '{fileName}'.");
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    return Result<int>.Failure($"Feil: Mappen til '{fileName}' finnes ikke.");
+                }
+                catch (IOException ex)
+                {
+                    return Result<int>.Failure($"Feil: Klarte ikke å skrive til '{fileName}'.\n" + ex.Message);
+                }
+            };
+        }
+        //public static Result<int> SaveFile(string fileName, int number)
+        //{
+        //    try
+        //    {
+        //        if (File.Exists(fileName))
+        //        {
+        //            return Result<int>.Failure($"Feil: Utfilen '{fileName}' finnes allerede.");
+        //        }
+
+        //        File.WriteAllText(fileName, number.ToString());
+
+        //        return Result<int>.Success(number);
+        //    }
+        //    catch (UnauthorizedAccessException)
+        //    {
+        //        return Result<int>.Failure($"Feil: Har ikke tilgang til å skrive til '{fileName}'.");
+        //    }
+        //    catch (DirectoryNotFoundException)
+        //    {
+        //        return Result<int>.Failure($"Feil: Mappen til '{fileName}' finnes ikke.");
+        //    }
+        //    catch (IOException ex)
+        //    {
+        //        return Result<int>.Failure($"Feil: Klarte ikke å skrive til '{fileName}'.\n" + ex.Message);
+        //    }
+        //}
+    }
+}
--- FileBasedAdd/Program.cs
+++ FileBasedAdd/Program.cs
@@ -1,103 +1,171 @@
-﻿using System;
+﻿
+using FileBasedAdd;
 
-if (args.Length != 3)
+
+var fileNamesResult = Logic.GetArgs(args);
+if (!fileNamesResult.IsSuccess)
 {
-    Console.Error.WriteLine("Feil: Du må angi nøyaktig tre filnavn.");
-    Console.Error.WriteLine("Bruk: dotnet run <tallfil1> <tallfil2> <resultatfil>");
+    Console.WriteLine(fileNamesResult.Error);
     return 1;
 }
 
-string inputFile1 = args[0];
-string inputFile2 = args[1];
-string outputFile = args[2];
+var sumResult =
+    Logic.ReadFileAndParseIntAndAdd(fileNamesResult.Value.InputFile1)(0)
+        .FlatMap(Logic.ReadFileAndParseIntAndAdd(fileNamesResult.Value.InputFile2))
+        .FlatMap(Logic.SaveFile(fileNamesResult.Value.OutputFile));
 
-int number1;
-int number2;
-
-try
+if (!sumResult.IsSuccess)
 {
-    string text1 = File.ReadAllText(inputFile1).Trim();
-
-    if (!int.TryParse(text1, out number1))
-    {
-        Console.Error.WriteLine($"Feil: Innholdet i '{inputFile1}' er ikke et gyldig heltall.");
-        Console.Error.WriteLine($"Innhold: '{text1}'");
-        return 1;
-    }
-}
-catch (FileNotFoundException)
-{
-    Console.Error.WriteLine($"Feil: Fant ikke filen '{inputFile1}'.");
-    return 1;
-}
-catch (UnauthorizedAccessException)
-{
-    Console.Error.WriteLine($"Feil: Har ikke tilgang til å lese filen '{inputFile1}'.");
-    return 1;
-}
-catch (IOException ex)
-{
-    Console.Error.WriteLine($"Feil: Klarte ikke å lese filen '{inputFile1}'.");
-    Console.Error.WriteLine(ex.Message);
+    Console.WriteLine(sumResult.Error);
     return 1;
 }
 
-try
-{
-    string text2 = File.ReadAllText(inputFile2).Trim();
+Console.WriteLine($"Skrev resultatet {sumResult.Value} til '{fileNamesResult.Value.OutputFile}'.");
+return 0;
 
-    if (!int.TryParse(text2, out number2))
-    {
-        Console.Error.WriteLine($"Feil: Innholdet i '{inputFile2}' er ikke et gyldig heltall.");
-        Console.Error.WriteLine($"Innhold: '{text2}'");
-        return 1;
-    }
-}
-catch (FileNotFoundException)
-{
-    Console.Error.WriteLine($"Feil: Fant ikke filen '{inputFile2}'.");
-    return 1;
-}
-catch (UnauthorizedAccessException)
-{
-    Console.Error.WriteLine($"Feil: Har ikke tilgang til å lese filen '{inputFile2}'.");
-    return 1;
-}
-catch (IOException ex)
-{
-    Console.Error.WriteLine($"Feil: Klarte ikke å lese filen '{inputFile2}'.");
-    Console.Error.WriteLine(ex.Message);
-    return 1;
-}
 
-int sum = number1 + number2;
+/*
+ * Omskriving til Result
+ */
 
-try
-{
-    if (File.Exists(outputFile))
-    {
-        Console.Error.WriteLine($"Feil: Utfilen '{outputFile}' finnes allerede.");
-        return 1;
-    }
 
-    File.WriteAllText(outputFile, sum.ToString());
+//var fileNamesResult = Logic.GetArgs(args);
+//if (!fileNamesResult.IsSuccess)
+//{
+//    Console.WriteLine(fileNamesResult.Error);
+//    return 1;
+//}
 
-    Console.WriteLine($"Skrev resultatet {sum} til '{outputFile}'.");
-    return 0;
-}
-catch (UnauthorizedAccessException)
-{
-    Console.Error.WriteLine($"Feil: Har ikke tilgang til å skrive til '{outputFile}'.");
-    return 1;
-}
-catch (DirectoryNotFoundException)
-{
-    Console.Error.WriteLine($"Feil: Mappen til '{outputFile}' finnes ikke.");
-    return 1;
-}
-catch (IOException ex)
-{
-    Console.Error.WriteLine($"Feil: Klarte ikke å skrive til '{outputFile}'.");
-    Console.Error.WriteLine(ex.Message);
-    return 1;
-}
+//var number1Result = Logic.ReadFileAndParseIntAndAdd(fileNamesResult.Value.InputFile1);
+//var number2Result = Logic.ReadFileAndParseIntAndAdd(fileNamesResult.Value.InputFile2);
+//if (!number1Result.IsSuccess)
+//{
+//    Console.WriteLine(number1Result.Error);
+//    return 1;
+//}
+
+//if (!number2Result.IsSuccess)
+//{
+//    Console.WriteLine(number2Result.Error);
+//    return 1;
+//}
+
+//int sum = number1Result.Value + number2Result.Value;
+
+//var outputFile = fileNamesResult.Value.OutputFile;
+//var saveFileResult = Logic.SaveFile(outputFile, sum);
+
+//if (!saveFileResult.IsSuccess)
+//{
+//    Console.WriteLine(saveFileResult.Error);
+//    return 1;
+//}
+
+//Console.WriteLine($"Skrev resultatet {saveFileResult.Value} til '{outputFile}'.");
+//return 0;
+
+/*
+ * Opprinnelig versjon
+ */
+
+//if (args.Length != 3)
+//{
+//    Console.Error.WriteLine("Feil: Du må angi nøyaktig tre filnavn.");
+//    Console.Error.WriteLine("Bruk: dotnet run <tallfil1> <tallfil2> <resultatfil>");
+//    return 1;
+//}
+
+//string inputFile1 = args[0];
+//string inputFile2 = args[1];
+//string outputFile = args[2];
+
+//int number1;
+//int number2;
+
+//try
+//{
+//    string text1 = File.ReadAllText(inputFile1).Trim();
+
+//    if (!int.TryParse(text1, out number1))
+//    {
+//        Console.Error.WriteLine($"Feil: Innholdet i '{inputFile1}' er ikke et gyldig heltall.");
+//        Console.Error.WriteLine($"Innhold: '{text1}'");
+//        return 1;
+//    }
+//}
+//catch (FileNotFoundException)
+//{
+//    Console.Error.WriteLine($"Feil: Fant ikke filen '{inputFile1}'.");
+//    return 1;
+//}
+//catch (UnauthorizedAccessException)
+//{
+//    Console.Error.WriteLine($"Feil: Har ikke tilgang til å lese filen '{inputFile1}'.");
+//    return 1;
+//}
+//catch (IOException ex)
+//{
+//    Console.Error.WriteLine($"Feil: Klarte ikke å lese filen '{inputFile1}'.");
+//    Console.Error.WriteLine(ex.Message);
+//    return 1;
+//}
+
+//try
+//{
+//    string text2 = File.ReadAllText(inputFile2).Trim();
+
+//    if (!int.TryParse(text2, out number2))
+//    {
+//        Console.Error.WriteLine($"Feil: Innholdet i '{inputFile2}' er ikke et gyldig heltall.");
+//        Console.Error.WriteLine($"Innhold: '{text2}'");
+//        return 1;
+//    }
+//}
+//catch (FileNotFoundException)
+//{
+//    Console.Error.WriteLine($"Feil: Fant ikke filen '{inputFile2}'.");
+//    return 1;
+//}
+//catch (UnauthorizedAccessException)
+//{
+//    Console.Error.WriteLine($"Feil: Har ikke tilgang til å lese filen '{inputFile2}'.");
+//    return 1;
+//}
+//catch (IOException ex)
+//{
+//    Console.Error.WriteLine($"Feil: Klarte ikke å lese filen '{inputFile2}'.");
+//    Console.Error.WriteLine(ex.Message);
+//    return 1;
+//}
+
+//int sum = number1 + number2;
+
+//try
+//{
+//    if (File.Exists(outputFile))
+//    {
+//        Console.Error.WriteLine($"Feil: Utfilen '{outputFile}' finnes allerede.");
+//        return 1;
+//    }
+
+//    File.WriteAllText(outputFile, sum.ToString());
+
+//    Console.WriteLine($"Skrev resultatet {sum} til '{outputFile}'.");
+//    return 0;
+//}
+//catch (UnauthorizedAccessException)
+//{
+//    Console.Error.WriteLine($"Feil: Har ikke tilgang til å skrive til '{outputFile}'.");
+//    return 1;
+//}
+//catch (DirectoryNotFoundException)
+//{
+//    Console.Error.WriteLine($"Feil: Mappen til '{outputFile}' finnes ikke.");
+//    return 1;
+//}
+//catch (IOException ex)
+//{
+//    Console.Error.WriteLine($"Feil: Klarte ikke å skrive til '{outputFile}'.");
+//    Console.Error.WriteLine(ex.Message);
+//    return 1;
+//}
